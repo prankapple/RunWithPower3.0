79f61c2364b440773023883a78fdb578edf6b051
prankapple-alt <prankapple2@gmail.com>
2026-01-31 11:44:29 +0200

    added the code

---
+++ RunWithPower/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace RunWithPower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Run As";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Not chosen",
+            "Invoker",
+            "Administrator",
+            "System",
+            "TrustedInstaller"});
+            this.comboBox1.Location = new System.Drawing.Point(68, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(270, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Location\\Filename";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(406, 142);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RunWithPower";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem idlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ RunWithPower/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+
+namespace RunWithPower
+{
+    public partial class Form1 : Form
+    {
+        private bool fileSelected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Executables (*.exe)|*.exe|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select a file to run";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+        }
+
+        private void ResetButton()
+        {
+            fileSelected = false;
+            button1.Text = "Browse";
+            textBox1.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // FIRST CLICK → Choose file
+            if (!fileSelected)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    button1.Text = "Run";
+                    fileSelected = true;
+                }
+                return;
+            }
+
+            // SECOND CLICK → Run file
+            if (!File.Exists(textBox1.Text))
+            {
+                MessageBox.Show("Selected file does not exist.");
+                ResetButton();
+                return;
+            }
+
+            string arguments;
+
+            switch (comboBox1.Text)
+            {
+                case "Not chosen":
+                    MessageBox.Show("Please choose an option!");
+                    return; // let user select, don't reset
+
+                case "Invoker":
+                    arguments = $"-i \"{textBox1.Text}\"";
+                    RunPsExec(arguments);
+                    break;
+
+                case "Administrator":
+                    arguments = $"-h \"{textBox1.Text}\"";
+                    RunPsExec(arguments);
+                    break;
+
+                case "System":
+                    arguments = $"-s -i \"{textBox1.Text}\"";
+                    RunPsExec(arguments);
+                    break;
+
+                case "TrustedInstaller":
+                    RunAsTrustedInstaller(textBox1.Text);
+                    break;
+
+                default:
+                    MessageBox.Show("Invalid option selected.");
+                    return;
+            }
+
+            // AFTER RUN → back to choose mode
+            ResetButton();
+        }
+
+        private void RunPsExec(string args)
+        {
+            try
+            {
+                if (!File.Exists("PsExec.exe"))
+                {
+                    MessageBox.Show("Error: PsExec.exe not found in the application folder!");
+                    return;
+                }
+
+                Process.Start("PsExec.exe", args);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error running PsExec: {ex.Message}");
+            }
+        }
+
+        private void RunAsTrustedInstaller(string applicationPath)
+        {
+            try
+            {
+                ServiceController sc = new ServiceController("TrustedInstaller");
+
+                if (sc.Status != ServiceControllerStatus.Running)
+                {
+                    sc.Start();
+                    sc.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(10));
+                }
+
+                Process[] proc = Process.GetProcessesByName("TrustedInstaller");
+
+                if (proc.Length > 0)
+                {
+                    IamYourDaddy.Run(proc[0].Id, applicationPath);
+                }
+                else
+                {
+                    MessageBox.Show("Error: TrustedInstaller process not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error running as TrustedInstaller: {ex.Message}");
+            }
+        }
+    }
+}
